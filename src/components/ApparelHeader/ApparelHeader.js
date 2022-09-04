dc21fdade970eb62d8d29eb641a037f3340bd38d
Aakash Verma <dev.aakashv@gmail.com>
2022-09-05 01:18:19 +0530

    format `ApparelHeader.js` file

--- src/components/ApparelHeader/ApparelHeader.js
+++ src/components/ApparelHeader/ApparelHeader.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import BaseApparelLogo from '../../assests/logo.svg';
-import styles from '../../css/components_styles/ApparelHeader.module.css';
+import React from "react";
+import BaseApparelLogo from "../../assests/logo.svg";
+import styles from "../../css/components_styles/ApparelHeader.module.css";
 
 const ApparelHeader = (props) => {
-    return (
-        <header className={styles.Header}>
-            <figure className={styles.Figure}>
-                <img src={BaseApparelLogo} alt="Base apparel logo" className={styles.Logo} />
-            </figure>
-        </header>
-    );
-}
+  return (
+    <header className={styles.Header}>
+      <figure className={styles.Figure}>
+        <img
+          src={BaseApparelLogo}
+          alt="Base apparel logo"
+          className={styles.Logo}
+        />
+      </figure>
+    </header>
+  );
+};
 
 export default ApparelHeader;
