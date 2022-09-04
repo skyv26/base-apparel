520425fbb6edbca6349ade92c232936d0f40b133
Aakash Verma <dev.aakashv@gmail.com>
2022-09-05 01:18:54 +0530

    format `ApparelInfo.js` file

--- src/components/ApparelInfo/ApparelInfo.js
+++ src/components/ApparelInfo/ApparelInfo.js
@@ -1,15 +1,23 @@
-import React, { Fragment } from 'react';
-import styles from '../../css/components_styles/ApparelInfo.module.css';
+import React, { Fragment } from "react";
+import styles from "../../css/components_styles/ApparelInfo.module.css";
 
 const ApparelInfo = (props) => {
-    return (
-        <Fragment>
-            <h1 className={styles.MainHeader}><span>We're</span><br />coming<br />soon</h1>
-            <p className={styles.MainPara}>Hello fellow shoppers! We're currently building our new fashion store.
-                Add your email below to stay up-to-date with announcements and our launch deals.
-            </p>
-        </Fragment>
-    );
-}
+  return (
+    <Fragment>
+      <h1 className={styles.MainHeader}>
+        <span>We're</span>
+        <br />
+        coming
+        <br />
+        soon
+      </h1>
+      <p className={styles.MainPara}>
+        Hello fellow shoppers! We're currently building our new fashion store.
+        Add your email below to stay up-to-date with announcements and our
+        launch deals.
+      </p>
+    </Fragment>
+  );
+};
 
 export default ApparelInfo;
