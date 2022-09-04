3e6fa502cac75efb99ee7d901470ac68966a1bbe
Aakash Verma <dev.aakashv@gmail.com>
2022-09-05 01:18:03 +0530

    format `ApparelFooter.js` file

--- src/components/ApparelFooter/ApparelFooter.js
+++ src/components/ApparelFooter/ApparelFooter.js
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const ApparelFooter = (props) => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  return <div></div>;
+};
 
 export default ApparelFooter;
