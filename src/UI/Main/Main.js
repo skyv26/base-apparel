4bd08adfe8117a2fba6592e9c40a430336517f13
Aakash Verma <dev.aakashv@gmail.com>
2022-09-12 12:20:01 +0530

    Resolve the `eslinter` error

--- src/UI/Main/Main.js
+++ src/UI/Main/Main.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import styles from '../../css/components_styles/Main.module.css';
-import Wrapper from '../Wrapper/Wrapper';
-import ApparelInfo from '../../components/ApparelInfo/ApparelInfo';
-import ApparelEmailContainer from '../../components/ApparelEmailContainer/ApparelEmailContainer';
+import React from "react";
+import styles from "../../css/components_styles/Main.module.css";
+import Wrapper from "../Wrapper/Wrapper";
+import ApparelInfo from "../../components/ApparelInfo/ApparelInfo";
+import ApparelEmailContainer from "../../components/ApparelEmailContainer/ApparelEmailContainer";
 
 const Main = (props) => {
-    return (
-        <main className={styles.Main}>
-            <Wrapper>
-                <ApparelInfo />
-                <ApparelEmailContainer />
-            </Wrapper>
-        </main>
-    );
-}
+  return (
+    <main className={styles.Main}>
+      <Wrapper>
+        <ApparelInfo />
+        <ApparelEmailContainer />
+      </Wrapper>
+    </main>
+  );
+};
 
 export default Main;
